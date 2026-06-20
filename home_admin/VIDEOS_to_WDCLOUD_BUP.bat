02c97e65e159d7fd9d02a325fa5285ed7370ec05
dbax <dan-aksenov@yandex.ru>
2026-06-20 10:51:15 +0300

    videos to ya

--- home_admin/VIDEOS_to_WDCLOUD_BUP.bat
+++ home_admin/VIDEOS_to_WDCLOUD_BUP.bat
@@ -5,6 +5,17 @@ set src="F:\Users\danila\Videos"
 
 IF NOT EXIST %log% MD %log%
 
+set dst="D:\Users\danila\YandexDisk\!BUPS\videos"
+
+rem get difference
+robocopy %src% %dst% *.* /s /purge /l /nfl /njh /ndl
+pause
+
+rem copy itself
+robocopy %src% %dst% *.* /unilog:%log%\video_to_mycloud.log /tee /z /purge /s
+ 
+pause
+
 set dst="\\wdmycloud\danila\!BUPS\Videos"
 
 rem get difference
